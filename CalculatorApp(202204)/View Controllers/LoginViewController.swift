9f7144554b7f91ddfc4bb3f3f18a765796f53b76
chechenliu <qwer21508@gmail.com>
2022-04-13 20:01:14 +0800

    feat：新增AutoLayout

--- CalculatorApp(202204)/View Controllers/LoginViewController.swift
+++ CalculatorApp(202204)/View Controllers/LoginViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    private var correctUserName = "rayliu"
+    private var correctUserName = "chiliwu"
     
     @IBOutlet weak var userNameTextField: UITextField!
     
